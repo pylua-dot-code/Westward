66b965f2c7639adb455f63e5aa89a7db29d086df
pylua.code <147281703+pylua-dot-code@users.noreply.github.com>
2024-05-22 16:33:26 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, world! wrong branch lol" << std::endl;
+    std::cout << "Hello, world! wrong branch lol. Wanna work now?" << std::endl;
     return 0;
 }
